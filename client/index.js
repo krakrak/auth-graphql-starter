7b3ccc371f2a29ac71582135b21cb2018dea4127
krakrak <jean-c-irak@hotmail.fr>
2018-04-08 15:38:39 +0200

    End Graphql Training

--- client/index.js
+++ client/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import {Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+import Dashboard from './components/Dashboard';
+import requireAuth from './components/requireAuth';
+
+const networkInterface = createNetworkInterface ({
+  uri: '/graphql',
+  opts: {
+    credentials:'same-origin'
+  }
+});
 
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 
 });
@@ -16,6 +28,9 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
+          <Route path="login" component={LoginForm}></Route>
+          <Route path="signup" component={SignupForm}></Route>
+          <Route path="dashboard" component= {requireAuth(Dashboard)}> </Route>
         </Route>
       </Router>
     </ApolloProvider>
